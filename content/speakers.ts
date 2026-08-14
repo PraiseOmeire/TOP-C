8c892995e1e1dbd5f49bb89da3b5f95d70cdef9f
Praise Omeire <praiseomeire@Praises-MacBook-Pro.local>
2026-08-14 11:06:14 +0100

    schedule

--- content/speakers.ts
+++ content/speakers.ts
@@ -34,9 +34,9 @@ export const speakers: Speaker[] = [
     imageSrc: "/images/speaker-1.jpeg",
   },
   {
-    slug: "pastor-david-eze",
-    name: "Pastor David Eze",
-    role: "Founder, Jesus Lovers Network Movement",
+    slug: "Evangelist-david-eze",
+    name: "Evangelist David Eze",
+    role: "Visioner, Jesus Lovers Network Movement",
     hook: "Shaping youth and teenagers in God's way through evangelism and outreach.",
     bio: "A minister and founder of the Jesus Lovers Network Movement. He is passionate about the lives of youth and teenagers, shaping them in God's way through evangelism and outreaches.",
     imageSrc: "/images/speaker-2.jpeg",
@@ -55,5 +55,5 @@ export const organizer: Organizer = {
   name: "Pst (Mrs) Rose Omeire",
   role: "Convener",
   note: "A devoted lover of God with a deep passion for raising and inspiring the next generation to know Christ, discover their identity in Him, and live purposeful lives for Jesus. She is the wife of the General Overseer of Voice of Life Evangelical Ministry (Full Life Assembly), Pastor Herbert N. Omeire, where she serves actively with dedication to the work of the ministry. With a special burden for teenagers, she serves as the Teenagers' Coordinator, mentoring, teaching, and guiding young people in their walk with Christ. She is the convener and host of the annual Teens of Purpose Conference, a platform created to help teenagers discover their God-given potential, develop their gifts, and understand that they were created for a divine purpose.",
-  imageSrc: "/images/host-3.jpg",
+  imageSrc: "/images/host-4.jpg",
 };
